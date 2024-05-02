1e20ad1959e717aa16e13993ac5c45579b2ca44f
Đàm Tiến Đạt <tdat0306@gmail.com>
2024-05-02 09:10:05 +0700

    Add templateStack.

--- My_first_Cpp_game/utils.cpp
+++ My_first_Cpp_game/utils.cpp
@@ -1,5 +1,25 @@
 #include "utils.h"
 
+template <typename T>
+bool Stack<T>::push(T x) {
+    if (top >= (MAXN - 1))
+        return false;
+    else {
+        a[++top] = x;
+        return true;
+    }
+}
+template <typename T>
+T Stack<T>::pop() {
+    if (top <= 0) {
+        return -INF;
+    }
+    else {
+        T x = a[top--];
+        return x;
+    }
+}
+
 int clamp(int min, int val, int max)
 {
 	if (val < min)  return min;
--- My_first_Cpp_game/utils.h
+++ My_first_Cpp_game/utils.h
@@ -9,5 +9,19 @@ typedef unsigned int u32;
 typedef long long s64;
 typedef unsigned long long u64;
 
+const int MAXN = (int)2e5 + 1;
+const long long INF = (long long)1e9;
+
+template <typename T>
+class Stack {
+    T top;
+    T a[MAXN];
+public:
+    Stack() { top = -1; }
+    bool push(T x);
+    T pop();
+    bool isEmpty() { return (top < 0); }
+};
+
 int clamp(int min, int val, int max);
 int random(int start, int end);
